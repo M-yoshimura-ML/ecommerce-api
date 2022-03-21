397b425a503961ace49471d528e7cd17e4742b5b
M-yoshimura-ML <masahiro.yoshimura@freemight.com>
2022-03-21 18:58:09 +0800

    not found exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,7 +4,10 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -38,6 +41,16 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+            if($request->expectsJson()){
+                if ($request->is('api/*')) {
+                    return response()->json([
+                        'message' => 'Not found.'
+                    ], Response::HTTP_NOT_FOUND);
+                }
+            }
         });
     }
 
